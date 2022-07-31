e31bfc2bd9562a05f977b63f7f934fc7c3f7b031
thanhvan181 <nguyenthivan18012000@gmail.com>
2022-07-31 10:23:39 +0700

    home and list

--- src/hackathon_semifinal_frontend/src/pages/Home/Home.jsx
+++ src/hackathon_semifinal_frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Button, Col, Row } from "antd"
+import { Link } from 'react-router-dom'
+import * as S from "./style"
+
 
 const Home = () => {
   return (
-      <div>
-          Home
-      </div>
-  )
+    <S.Wrapper>
+      <Row>
+        <Col span={16}>
+          <Link to={"/list"}>Customer lists</Link>
+        </Col>
+        <Col span={8}>
+          <Button>Connect</Button>
+        </Col>
+      </Row>
+    </S.Wrapper>
+  );
 }
 
 export default Home
+
